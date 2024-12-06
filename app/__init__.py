3161ab33e770a9abf8e0f7c15759f674bc94b279
Suhana Kumar <skumar50@stuy.edu>
2024-12-06 11:20:22 -0500

    working on login page

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,29 @@ app.secret_key = key.key()
 def login():
     return render_template("login.html")
 
+@app.route("/login",  methods=['GET','POST'])
+def disp_loginpage():
+    if 'email' and 'password' in session:
+        name = session['email']
+        return render_template('homepage.html', user=name)
+    return render_template( 'login.html' ) #renders homepage
+
+
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    session.pop('email', None)
+    session.pop('password', None)
+    return render_template('logout.html')
+
+
+@app.route("/homepage", methods = ['GET', 'POST'])
+def redirect():
+    session['email'] = request.form['email']
+    session['password'] = request.form['password']
+    name = session['email']
+    return render_template('homepage.html', user=name)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
